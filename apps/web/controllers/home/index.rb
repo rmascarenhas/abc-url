ccc3b70de93a31d5458bf724c32d31ff7441a071
Renato Mascarenhas <renato@fastmail.jp>
2017-01-12 16:45:04 -0200

    Render a Welcome page at the root of the web app.

---
+++ apps/web/controllers/home/index.rb
@@ -0,0 +1,14 @@
+module Web::Controllers::Home
+
+  # Web::Controllers::Home
+  #
+  # The controller for the root of the URL shortening app. No parameters are
+  # expected in this endpoint.
+  class Index
+    include Web::Action
+
+    def call(params)
+    end
+  end
+
+end
---
+++ apps/web/views/home/index.rb
@@ -0,0 +1,9 @@
+module Web::Views::Home
+
+  # The +Index+ page only renders the HTML template located at
+  # +app/templates/home/index.thml.erb+
+  class Index
+    include Web::View
+  end
+
+end
