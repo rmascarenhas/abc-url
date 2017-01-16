8926e0e0088a7d20f7f86e574064991047a9ef66
Renato Mascarenhas <renato@fastmail.jp>
2017-01-16 17:14:46 +0800

    Make an API call when the user fills in the main form.

--- apps/web/assets/javascripts/application.js
+++ apps/web/assets/javascripts/application.js
@@ -1,6 +1,22 @@
 $(document).ready(function() {
   var countdownEl = $("#reload-countdown");
   var seconds     = 15;
+  var abcForm     = $("#abc-form");
+
+  // when the URL shortening form is submitted, make an API call to save the URL
+  // and get its ID back from the server. With that, we can redirect the user to
+  // the page with the clicks information
+  //
+  // If there is an error processing the request, show a message to the user indicating
+  // that the provided URL is not valid
+  abcForm.submit(function(event) {
+    var url = $(this).serializeArray()[0].value;
+    $.post("/api/shorten", { url: url })
+      .done(function(data) { window.location = window.location + 'url/' + data.id; })
+      .fail(function() { $('.url-error').text('This does not look like a valid URL') });
+
+    event.preventDefault();
+  });
 
   // sets the content of the element with ID +reload-countdown+ to a message
   // indicating in how many seconds the page will be reloaded.
